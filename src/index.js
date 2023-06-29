e8398889fa09892b9e0ac34685dad30c63ae963c
talant <talant.04asanov@gmail.com>
2023-06-29 12:28:04 +0600

    finished

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const rem = 16
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: rem * 2,
+});
